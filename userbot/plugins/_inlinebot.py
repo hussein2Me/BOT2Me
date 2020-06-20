fadf76ea9445a8157d2bde8425d2fec81520d993
hussein2Me <66622941+hussein2Me@users.noreply.github.com>
2020-06-20 15:36:12 +0300

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -18,7 +18,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© Userbot Help",
-                text="{}\nCurrently Loaded Plugins: {}".format(
+                text="{}\nالاضافات المثبته حاليا: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
+            reply_pop_up_alert = "يرجى الحصول على البوت الخاص بك , لا تستخدم هذا ملكي"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -55,7 +55,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Please get your own Userbot, join @HardcoreUserbot. Don't use mine!"
+            reply_pop_up_alert = "الرجاء الانضمام الى قناة المساعدة @TEAM2ME"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
@@ -70,10 +70,10 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         except:
             pass
         if help_string is "":
-            reply_pop_up_alert = "{} is useless".format(plugin_name)
+            reply_pop_up_alert = "{} غير قانوني".format(plugin_name)
         else:
             reply_pop_up_alert = help_string
-        reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
+        reply_pop_up_alert += "\n استخدم .unload {} لحذف هذا الاضافه\n\
             © Userbot".format(plugin_name)
         try:
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
